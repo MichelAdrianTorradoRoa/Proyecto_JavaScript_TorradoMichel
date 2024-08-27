df0a5d03c268efe7678e96a1f32e2ad64ada1aca
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2024-08-27 17:01:33 -0500

    feat: :construction: En construccion
    by @MichelAdrianTorradoRoa

--- page.js
+++ page.js
@@ -1,16 +1,14 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Obtener los valores del formulario
-    const username = document.getElementById('username').value;
+    
+    const serie = document.getElementById('serie').value;
     const password = document.getElementById('password').value;
 
     // Aquí deberías validar las credenciales del usuario con un backend real.
     // Para este ejemplo, simplemente verificaremos si el usuario y la contraseña no están vacíos.
-    if (username && password) {
+    if (serie) {
         // Redirigir a la página principal si el inicio de sesión es exitoso.
-        window.location.href = 'page.html';
-    } else {
-        alert('Por favor, ingrese un usuario y una contraseña válidos.');
+        window.location.href = 'serie.html';
     }
 });
